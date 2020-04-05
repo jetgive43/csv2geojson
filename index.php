6c84f5286bd2ebb0daa68a8c7b0ffda08ab1a6de
automarketing <auto_marketing@outlook.com>
2020-04-05 21:20:57 +0300

    add'

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+if($_POST)
+{
+    $a1=0;
+    move_uploaded_file($_FILES['fileToUpload']['tmp_name'],"geo.csv");
+    $file = fopen("geo.csv","r");
+    $csv_array=fgetcsv($file,1000,",");
+    while (($data = fgetcsv($file, 1000, ",")) !== FALSE)
+    {
+        $feature[]=[
+            'type' => 'Feature',
+            'geometry' => array(
+                'type' => 'LineString',
+                # Pass Longitude and Latitude Columns here
+                'coordinates' => [[(float)$data[1],(float)$data[2]],[(float)$data[6],(float)$data[7]]]
+            ),
+            # Pass other attribute columns here
+            'properties' => [''=>'']
+        ];
+    }
+    $geojson = array(
+        'type'      => 'FeatureCollection',
+        'features'  => $feature
+    );
+    fclose($h);
+    echo(json_encode($geojson));
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="geo.php" method="post" enctype="multipart/form-data">
+    Select GEO CSV to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload GEO CSV" name="submit">
+</form>
+
+</body>
+</html>
